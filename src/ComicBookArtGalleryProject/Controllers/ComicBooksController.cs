05de0ab094a26c77d86828def54a33c2a08c4822
christianamaury <chris.amaury@hotmail.com>
2021-04-03 09:02:07 -0700

    Added Comic Books Controller

---
+++ src/ComicBookArtGalleryProject/Controllers/ComicBooksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//Using directive has been added in order to use "Controller" base class
+using System.Web.Mvc;
+
+namespace ComicBookArtGalleryProject.Controllers
+{
+
+    //Updating class to inherit from the MVC controller base class
+    public class ComicBooksController: Controller
+    {
+
+    }
+}
